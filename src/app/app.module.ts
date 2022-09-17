1f8706792fc01ed12e3ed2a590016df7e3be6b70
lbentancour <lbentancour@cent.gov.ar>
2022-09-16 23:11:32 -0300

    arreglo cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { ToolbarComponent } from './componentes/toolbar/toolbar.component';
+import { StudentComponent } from './componentes/student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule
