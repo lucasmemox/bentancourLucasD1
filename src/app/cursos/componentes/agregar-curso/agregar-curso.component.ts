120a4f3cfd1059cf7a67c77575203b48d3c80c99
lbentancour <lbentancour@cent.gov.ar>
2022-11-07 00:18:43 -0300

    agrego el redirigir a la vista cursos

--- src/app/cursos/componentes/agregar-curso/agregar-curso.component.ts
+++ src/app/cursos/componentes/agregar-curso/agregar-curso.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { CursoService } from '../../servicios/curso.service';
+import { Curso } from '../../models/curso';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agregar-curso',
@@ -7,7 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarCursoComponent implements OnInit {
 
-  constructor() { }
+  formulario: FormGroup;
+
+  constructor(
+    private CursoService: CursoService,
+    private router: Router
+  ) {
+    this.formulario = new FormGroup({
+      nombre : new FormControl(),
+      comision: new FormControl(),
+      profesor: new FormControl(),
+      fechaInicio: new FormControl(),
+      fechaFin: new FormControl(),
+      inscripcionAbierta: new FormControl()
+    });
+   }
+
+   agregarCurso(){
+
+    const curso: Curso = {
+      id: Math.round(Math.random()*1000),
+      nombre: this.formulario.value.nombre,
+      comision: this.formulario.value.comision,
+      profesor: this.formulario.value.profesor,
+      fechaInicio: this.formulario.value.fechaInicio,
+      fechaFin: this.formulario.value.fechaFin,
+      inscripcionAbierta: this.formulario.value.inscripcionAbierta,
+      imagen: './assets/imagenes/angular-curso.png',
+    };
+
+    this.CursoService.agregarCurso(curso);
+    this.router.navigate(['cursos'])
+  }
 
   ngOnInit(): void {
   }
