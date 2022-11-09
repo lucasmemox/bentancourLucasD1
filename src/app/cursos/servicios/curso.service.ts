e129ab21f7ad5588aed897f1aee4635ecac64191
lbentancour <lbentancour@cent.gov.ar>
2022-11-09 19:33:07 -0300

    agrego el shared y modifico el curso service

--- src/app/cursos/servicios/curso.service.ts
+++ src/app/cursos/servicios/curso.service.ts
@@ -3,23 +3,24 @@ import { BehaviorSubject, map, Observable } from 'rxjs';
 import { Curso } from '../models/curso';
 import { Datos } from '../datos/cursos';
 
-@Injectable()
-
+@Injectable({
+  providedIn: 'root',
+})
 export class CursoService {
   private cursos: Curso[] = Datos.cursos;
   private cursosSubject: BehaviorSubject<Curso[]>;
 
   constructor() {
-   this.cursosSubject = new BehaviorSubject<Curso[]>(this.cursos);
+    this.cursosSubject = new BehaviorSubject<Curso[]>(this.cursos);
   }
 
-  obtenerCursos(): Observable<Curso[]>{
+  obtenerCursos(): Observable<Curso[]> {
     return this.cursosSubject.asObservable();
   }
 
-  obtenerCurso(id: number): Observable<Curso[]>{
+  obtenerCurso(id: number): Observable<Curso>{
     return this.obtenerCursos().pipe(
-      map((cursos: Curso[]) => this.cursos.filter((Curso: Curso) => Curso.id === id))
+      map((cursos: Curso[]) => cursos.filter((curso: Curso) => curso.id === id)[0])
     )
   }
 
@@ -29,10 +30,22 @@ export class CursoService {
   }
 
   editarCurso(curso: Curso){
+    let indice = this.cursos.findIndex((c: Curso) => c.id === curso.id);
 
+    if(indice > -1){
+      this.cursos[indice] = curso;
+    }
+
+    this.cursosSubject.next(this.cursos);
   }
 
   eliminarCurso(id: number){
+    let indice = this.cursos.findIndex((c: Curso) => c.id === id);
 
+    if(indice > -1){
+      this.cursos.splice(indice, 1);
+    }
+
+    this.cursosSubject.next(this.cursos);
   }
 }
