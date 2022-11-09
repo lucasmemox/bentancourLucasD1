acbdd7f0ce8bfa50b8e9a88e411b9fa747ab5a55
lbentancour <lbentancour@cent.gov.ar>
2022-11-08 23:34:21 -0300

    empiezo a renderizar las rutas

--- src/app/cursos/cursos-routing.module.ts
+++ src/app/cursos/cursos-routing.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarCursoComponent } from './componentes/agregar-curso/agregar-curso.component';
 import { CursosComponent } from './componentes/listar-cursos/cursos.component';
 
-const routes: Routes = [
-  // { path: 'cursos', component: CursoInicioComponent, children: [
-    { path: 'listar', component: CursosComponent},
-    // { path: 'editar', component: EditarCursosComponent},
-    // { path: 'agregar', component: AgregarCursosComponent}
-//  ]
-// }
+const rutas: Routes = [
+    // { path: 'listar', component: CursosComponent},
+    // { path: 'editar', component: EditarCursoComponent },
+    // { path: 'agregar_curso', component: AgregarCursoComponent}
+
+    { path: 'listar', component: CursosComponent, children: [
+      // { path: 'listar', component: ListaCursosComponent },
+      // { path: 'editar', component: EditarCursoComponent },
+      { path: 'agregar', component: AgregarCursoComponent }
+    ]}
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(rutas)],
   exports: [RouterModule]
 })
 export class CursosRoutingModule { }
