f416cd245ea0086a67741be70450f0caa44e902a
lbentancour <lbentancour@cent.gov.ar>
2022-11-09 19:44:08 -0300

    modifico los modulos material y app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,33 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/componentes/navbar/navbar.component';
 import { ToolbarComponent } from './core/componentes/toolbar/toolbar.component';
-import { StudentComponent } from './componentes/student/student.component';
-import { FooterComponent } from './core/componentes/footer/footer.component';
-import { FormularioAlumnoComponent } from './componentes/formulario-alumno/formulario-alumno.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from './material.module';
 import { AppRoutingModule } from './app.routing.module';
-import { CursosModule } from './cursos/cursos.module';
+import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import { CursosModule } from './cursos/cursos.module';
+
+// import { StudentComponent } from './componentes/student/student.component';
+// import { FormularioAlumnoComponent } from './componentes/formulario-alumno/formulario-alumno.component';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { MaterialModule } from './material.module';
+import { FooterComponent } from './core/componentes/footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ToolbarComponent,
-    StudentComponent,
-    FooterComponent,
-    FormularioAlumnoComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    CursosModule,
     CoreModule,
-    ReactiveFormsModule,
-    FormsModule,
-    MaterialModule,
+    CursosModule,
+    SharedModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,12 +4,14 @@ import { MatTableModule} from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDialogModule } from '@angular/material/dialog'
+
+import { MatDialogModule } from '@angular/material/dialog';
+import {MatSelectModule} from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
     imports: [
@@ -20,8 +22,10 @@ import { MatDialogModule } from '@angular/material/dialog'
         MatInputModule,
         MatIconModule,
         MatDialogModule,
+        MatSelectModule,
         MatDatepickerModule,
-        MatSlideToggleModule
+        MatSlideToggleModule,
+        MatNativeDateModule
     ],
     exports: [
         MatTableModule,
@@ -30,9 +34,11 @@ import { MatDialogModule } from '@angular/material/dialog'
         MatFormFieldModule,
         MatInputModule,
         MatIconModule,
+        MatDialogModule,
+        MatSelectModule,
         MatDatepickerModule,
         MatSlideToggleModule,
-        MatDialogModule
+        MatNativeDateModule
     ]
 })
 export class MaterialModule{}
