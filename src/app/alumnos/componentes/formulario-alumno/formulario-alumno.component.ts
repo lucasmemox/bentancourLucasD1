3753c35282a48e4f359056972d51975c0433b63e
lbentancour <lbentancour@cent.gov.ar>
2022-11-11 23:29:49 -0300

    quito coma innecesaria

--- src/app/alumnos/componentes/formulario-alumno/formulario-alumno.component.ts
+++ src/app/alumnos/componentes/formulario-alumno/formulario-alumno.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder,FormControl, FormGroup,Validators} from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
