c49e9c9d3fd02ee1fa58cb0f42a6613dd7a693eb
lbentancour <lbentancour@cent.gov.ar>
2022-11-17 17:44:08 -0300

    agrego modificaciones al routing

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -5,24 +5,52 @@ import { InicioComponent } from './core/componentes/inicio/inicio.component';
 import { PaginaNoEncontradaComponent } from './core/componentes/pagina-no-encontrada/pagina-no-encontrada.component';
 import { QuienesSomosComponent } from './core/componentes/quienes-somos/quienes-somos.component';
 import { IdentificarmeGuard } from './core/guards/identificarme.guard';
+import { EditarCursoComponent } from './cursos/componentes/editar-curso/editar-curso.component';
 import { IdentificarmeModule } from './identificarme/identificarme.module';
 
 const rutas: Routes = [
-  { path: 'inicio', component: InicioComponent, canActivate: [IdentificarmeGuard] },
-  { path: 'contacto', component: ContactoComponent, canActivate: [IdentificarmeGuard] },
-  { path: 'quienes-somos', component: QuienesSomosComponent, canActivate: [IdentificarmeGuard] },
+  {
+    path: 'inicio',
+    component: InicioComponent,
+    canActivate: [IdentificarmeGuard],
+  },
+  {
+    path: 'contacto',
+    component: ContactoComponent,
+    canActivate: [IdentificarmeGuard],
+  },
+  {
+    path: 'quienes-somos',
+    component: QuienesSomosComponent,
+    canActivate: [IdentificarmeGuard],
+  },
 
-  { path: 'cursos', loadChildren: () =>import('./cursos/cursos.module').then((m)=> m.CursosModule), canActivate:[IdentificarmeGuard]},
-    { path: 'alumnos', loadChildren: ()=>import('./alumnos/alumnos.module').then((m)=>m.AlumnosModule), canActivate:[IdentificarmeGuard]},
-    { path: 'autenticacion', loadChildren: ()=>import('./identificarme/identificarme.module').then((m)=>m.IdentificarmeModule)},
+  {
+    path: 'cursos',
+    loadChildren: () =>
+      import('./cursos/cursos.module').then((m) => m.CursosModule),
+    canActivate: [IdentificarmeGuard],
+  },
+  {
+    path: 'alumnos',
+    loadChildren: () =>
+      import('./alumnos/alumnos.module').then((m) => m.AlumnosModule),
+    canActivate: [IdentificarmeGuard],
+  },
+  {
+    path: 'autenticacion',
+    loadChildren: () =>
+      import('./identificarme/identificarme.module').then(
+        (m) => m.IdentificarmeModule
+      ),
+  },
 
-  { path: '', redirectTo: 'inicio', pathMatch: 'full'},
-  { path: '**', component: PaginaNoEncontradaComponent }
-]
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: '**', component: PaginaNoEncontradaComponent },
+];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(rutas)],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(rutas)],
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule{}
+export class AppRoutingModule {}
