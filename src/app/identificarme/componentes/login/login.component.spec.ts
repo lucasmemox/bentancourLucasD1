c1582cf32d89db151bf15060bf04ce01e5700a6c
lbentancour <lbentancour@cent.gov.ar>
2022-11-20 00:25:26 -0300

    agrego el test del login

--- src/app/identificarme/componentes/login/login.component.spec.ts
+++ src/app/identificarme/componentes/login/login.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,7 +9,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports:[ReactiveFormsModule, MatSlideToggleModule]
     })
     .compileComponents();
 
@@ -20,4 +22,31 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+   it('Debe retornar formulario invalido', () => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    const app = fixture.componentInstance;
+    fixture.detectChanges();
+
+    const usuario = app.formulario.controls['usuario'];
+    usuario.setValue('admin');
+
+    expect(app.formulario.invalid).toBeTrue();
+  });
+
+  it('Debe retornar formulario valido', () => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    const app = fixture.componentInstance;
+    fixture.detectChanges();
+
+    let usuario = app.formulario.controls['usuario'];
+    let clave = app.formulario.controls['clave'];
+
+    usuario.setValue('admin');
+    clave.setValue('admin');
+
+
+    expect(app.formulario.invalid).toBeFalse();
+  });
+
 });
