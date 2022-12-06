c444924f8de47da08b97fba0373b592304b22927
lbentancour <lbentancour@cent.gov.ar>
2022-12-06 00:57:22 -0300

    aaa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/componentes/navbar/navbar.component';
@@ -9,8 +9,11 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { FooterComponent } from './core/componentes/footer/footer.component';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 
-@NgModule({
+NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
@@ -23,9 +26,12 @@ import { StoreModule } from '@ngrx/store';
     CoreModule,
     SharedModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
